f7249023aa6498acf5246a7bd68feba1d65ade91
Rocken <r0ck3n@hotmail.com>
2022-10-08 18:36:29 +0200

    typage onCLick ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import React from "react";
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
-function App() {
+const useIncrement = (initialValue = 0, step = 1) => {
+  const [count, setCount] = useState<any>(initialValue);
+  console.log(useState);
+
+  const increment = () => {
+    setCount((c: number) => c + step);
+  };
+  return [count, increment];
+};
+
+function Compteur() {
+  const [count, increment] = useIncrement(10);
+
+  return (
+    <div className="flex gap30">
+      <button className="btn" onClick={increment}>
+        Incrementer
+      </button>
+      <p>{String(count)}</p>
+    </div>
+  );
+}
+
+function App(): JSX.Element {
   return (
     <div>
-      <h1>Hello Word</h1>
+      <Compteur />
     </div>
   );
 }
